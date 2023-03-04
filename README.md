af11c98be38d6bd109cee3d3da12fb7ba3e4f6a2
Deby Novoa <103707560+Yohan-Black@users.noreply.github.com>
2023-03-03 21:38:22 -0500

    Alioth: Bringup for SkylineUI Project
    - Use aosp_alioth as per our vendor/aosp bringup
    - Use Proper kernel config, required to be used with Immensity kernel
    - Remove lineage.dependencies fine, add aosp.dependencies as per our project
    - Import my [Yohan_Black] SkylineUI Maintainer Flags

---
+++ README.md
@@ -0,0 +1,9 @@
+# Device tree repository for Alioth/in (Poco F3 / Mi 11x / Redmi K40)
+
+```
+#
+# Copyright (C) 2023 SkylineUI Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 SkyeliUI Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common SkyeliUI stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
@@ -30,5 +31,5 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP  := true
 
 # SkyeliUI Maintainer Flags
-VOID_MAINTAINER := Yohan_Black
+SKYLINEUI_MAINTAINER := Yohan_Black
 CUSTOM_BUILD_TYPE := OFFICIAL
